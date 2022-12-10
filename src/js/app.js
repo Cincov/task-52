2298f572ce22d5df54a39d83ef9fb179f8836982
Cincov <76522472+Cincov@users.noreply.github.com>
2022-12-10 10:53:55 +0200

    first

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,27 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  var count = 0;
+
+  document.body.addEventListener("click", () => {
+
+    for(let i = 0; i < 5; i++) {
+      let x = document.createElement("article");
+       
+      count += length;
+
+      x.setAttribute("id", "myArticle" + count)
+      document.appendChild(x);
+
+      let text = "some text..." + count;
+
+      document.getElementById("myArticle" + count).innerText = text;
+
+      document.getElementById("myArticle" + count).classList.add("message");
+    }
+
+  })
+
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
